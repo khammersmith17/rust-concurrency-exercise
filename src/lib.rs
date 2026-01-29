e647f6c4c93e4a8fd9eb93307923c332c1561284
khammersmith17 <kilianhammersmith@gmail.com>
2026-01-29 09:45:23 -0500

    adding naive and type safe oneshot channel implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::cell::UnsafeCell;
+use std::collections::VecDeque;
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Condvar, Mutex};
 
 /// This is like a tradional Mutex, or a non rust Mutex.
 /// Can lock around code blocks, not data like Rust Concurrency primitives typically do.
@@ -84,6 +86,210 @@ impl<T> DataSpinLock<T> {
         SpinLockGuard { lock: self }
     }
 }
+/// This basic channel uses a VecDeque internally to queue data, and a condition variable to notify
+/// receiver when data is ready.
+/// This channel is unbounded.
+pub struct BasicChannel<T> {
+    data: Mutex<VecDeque<T>>,
+    ready: Condvar,
+}
+
+impl<T> BasicChannel<T> {
+    pub fn new() -> BasicChannel<T> {
+        BasicChannel {
+            data: Mutex::new(VecDeque::new()),
+            ready: Condvar::new(),
+        }
+    }
+
+    pub fn send(&self, data: T) {
+        self.data.lock().unwrap().push_back(data);
+        self.ready.notify_one();
+    }
+
+    /// This is a blocking implementation of recv.
+    pub fn recv_blocking(&self) -> T {
+        let mut guard = self.data.lock().unwrap();
+        loop {
+            if let Some(message) = guard.pop_front() {
+                return message;
+            } else {
+                guard = self.ready.wait(guard).unwrap();
+            }
+        }
+    }
+
+    /// Non blocking recv returns an option if data is available.
+    pub fn recv_non_blocking(&self) -> Option<T> {
+        self.data.lock().unwrap().pop_front()
+    }
+}
+pub mod basic_one_shot_channel {
+    use std::cell::UnsafeCell;
+    use std::mem::MaybeUninit;
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    #[derive(Debug)]
+    pub struct Channel<T> {
+        data: UnsafeCell<MaybeUninit<T>>,
+        // 2 booleans defines 4 possible states, it may also make sense to use an enum in this case
+        used: AtomicBool,
+        ready: AtomicBool,
+    }
+
+    unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+    impl<T: std::fmt::Debug> Channel<T> {
+        pub fn new() -> Channel<T> {
+            Channel {
+                data: UnsafeCell::new(MaybeUninit::uninit()),
+                used: AtomicBool::new(false),
+                ready: AtomicBool::new(false),
+            }
+        }
+
+        pub fn is_ready(&self) -> bool {
+            // relaxed ordering here since we do not need a well defined happens before
+            // because of the fact no data is read here, and a single atomic is updated which
+            // preserves total modifcation order
+            self.ready.load(Ordering::Relaxed)
+        }
+
+        pub fn in_use(&self) -> bool {
+            // same considerations as is_ready
+            self.used.load(Ordering::Relaxed)
+        }
+
+        /// SAFETY: assumes only a single caller calls once.
+        /// Panics when the channel has been used, ie data has been written.
+        pub fn send(&self, message: T) {
+            // Ensure that the channel is not in use by swapping a true here and ensure the
+            // previous value in this atomic was false, otherwise panic
+            assert!(!self.used.swap(true, Ordering::Relaxed));
+
+            unsafe { (*self.data.get()).write(message) };
+
+            // update the ready flag using release store ordering
+            self.ready.store(true, Ordering::Release);
+        }
+
+        /// SAFETY: It is up to the user here to ensure that the data is ready to be consumed.
+        pub fn recv(&self) -> T {
+            // swap in a false for ready flag and assert that the previous value was true.
+            assert!(self.ready.swap(false, Ordering::Acquire));
+
+            // Compiler suggests explicit unsafe block here
+            // Even though we are in an unsafe function
+            let message = unsafe { (*self.data.get()).assume_init_read() };
+            message
+        }
+    }
+
+    impl<T> Drop for Channel<T> {
+        fn drop(&mut self) {
+            // we only need to drop the data if has been written but not read
+            if *self.ready.get_mut() {
+                unsafe { self.data.get_mut().assume_init_drop() }
+            }
+        }
+    }
+}
+
+pub mod type_safe_oneshot_channel {
+    use std::cell::UnsafeCell;
+    use std::mem::MaybeUninit;
+    use std::sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    };
+
+    pub fn channel<T>() -> (Sender<T>, Reciever<T>) {
+        let sender_arc: Arc<Channel<T>> = Arc::new(Channel::new());
+        let reciever_arc = Arc::clone(&sender_arc);
+
+        (
+            Sender {
+                channel: sender_arc,
+            },
+            Reciever {
+                channel: reciever_arc,
+            },
+        )
+    }
+
+    pub struct Reciever<T> {
+        channel: Arc<Channel<T>>,
+    }
+
+    impl<T> Reciever<T> {
+        pub fn is_ready(&self) -> bool {
+            self.channel.is_ready()
+        }
+
+        // move self so it is dropped when a user calls recv().
+        // this prevents any additional attempt to read the data at compile time
+        pub fn recv(self) -> T {
+            self.channel.read_message()
+        }
+    }
+
+    pub struct Sender<T> {
+        channel: Arc<Channel<T>>,
+    }
+
+    impl<T> Sender<T> {
+        // move self to guarante that only one message is passed. Sender is dropped after the
+        // message is consumed and written to the channel
+        pub fn send(self, message: T) {
+            self.channel.write_message(message);
+        }
+    }
+
+    // The channel is not private, and all actions are done via the publicly exposed.
+    struct Channel<T> {
+        message: UnsafeCell<MaybeUninit<T>>,
+        ready: AtomicBool,
+    }
+
+    unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+    impl<T> Channel<T> {
+        fn new() -> Channel<T> {
+            Channel {
+                message: UnsafeCell::new(MaybeUninit::uninit()),
+                ready: AtomicBool::new(false),
+            }
+        }
+
+        fn is_ready(&self) -> bool {
+            self.ready.load(Ordering::Relaxed)
+        }
+
+        fn write_message(&self, message: T) {
+            debug_assert!(!self.ready.load(Ordering::Acquire));
+
+            unsafe { (*self.message.get()).write(message) };
+            self.ready.store(true, Ordering::Release);
+        }
+
+        fn read_message(&self) -> T {
+            // need to set ready back to false so no attempt is made to drop the data inside the
+            // MaybeUninit. Using an atomic swap here.
+            debug_assert!(self.ready.swap(false, Ordering::Acquire));
+
+            unsafe { (*self.message.get()).assume_init_read() }
+        }
+    }
+
+    impl<T> Drop for Channel<T> {
+        fn drop(&mut self) {
+            // we only need to drop the data if has been written but not read
+            if *self.ready.get_mut() {
+                unsafe { self.message.get_mut().assume_init_drop() }
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -105,5 +311,24 @@ mod test {
 
         let g = data.lock();
         assert!(g.as_slice() == &[1, 2, 2] || g.as_slice() == &[2, 2, 1]);
+    }
+
+    #[test]
+    fn basic_channel_test() {
+        use basic_one_shot_channel::Channel;
+        let channel: Channel<String> = Channel::new();
+        let t = thread::current();
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                channel.send(String::from("hello world"));
+                t.unpark()
+            });
+            while !channel.is_ready() {
+                thread::park()
+            }
+        });
+
+        assert_eq!(channel.recv(), String::from("hello world"))
     }
 }
